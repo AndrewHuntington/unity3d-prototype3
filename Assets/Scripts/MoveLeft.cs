330172b8832b2b4bbd8d7735d9a5c5803d4c1902
Andrew Huntington <andrew.huntington@gmail.com>
2023-12-28 00:01:15 +0900

    Destory obstacles that exit bounds - Finish Lesson 3.2

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -7,6 +7,7 @@ public class MoveLeft : MonoBehaviour
 {
   private float speed = 30;
   private PlayerController playerControllerScript;
+  private float leftBound = -15;
 
   // Start is called before the first frame update
   void Start()
@@ -20,6 +21,11 @@ public class MoveLeft : MonoBehaviour
     if (!playerControllerScript.gameOver)
     {
       transform.Translate(speed * Time.deltaTime * Vector3.left);
+    }
+
+    if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+    {
+      Destroy(gameObject);
     }
   }
 }
